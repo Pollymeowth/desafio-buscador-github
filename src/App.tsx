85b31c8c472e6c3e7be6fcf59d090fc89dd9cd50
Pollyana Roberta <polly4dev@gmail.com>
2026-03-31 12:16:44 -0300

    Configurações iniciais do projeto / Teste chakra

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Button colorPalette="teal">Teste</Button>
+    </div>
+  );
+}
+
+export default App;
