5d8ec3f1219bb9af8957128aa9850dec9f870980
Pollyana Roberta <polly4dev@gmail.com>
2026-03-31 16:57:21 -0300

    buscar e mostrar usuario no Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,38 @@
-export function Profile(){
-    return <h1> Profile </h1>
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getUser } from "../services/github";
+
+export function Profile() {
+    const { username } = useParams();
+    const [user, setUser] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchUser() {
+            if (!username) return;
+
+            try {
+                const data = await getUser(username);
+                setUser(data);
+            } catch (error) {
+                console.log("Error fetching user")
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchUser();
+    }, [username]);
+
+    if (loading) return <p>Loading...</p>
+
+    return (
+        <div>
+            <img src={user?.avatar_url} width={100} />
+            <h1>{user?.name}</h1>
+            <p>{user?.bio}</p>
+        </div>
+    );
+
+
+
 }
